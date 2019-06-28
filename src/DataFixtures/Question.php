e88aeaf6ca2f0f0d4b8f2db34b7c1bfdbfcf97e4
Maxime Vasseur <zertouflex@gmail.com>
2019-06-28 02:47:46 +0200

    fixtures questions

--- src/DataFixtures/Question.php
+++ src/DataFixtures/Question.php
@@ -10,14 +10,47 @@ namespace App\DataFixtures;
 
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class Question extends Fixture
+class Question extends Fixture implements DependentFixtureInterface
 {
+    public function getDependencies()
+    {
+        return [Category::class];
+    }
+
     public function load(ObjectManager $manager)
     {
-         $question = new Question();
-         $manager->persist($question);
+         $question1 = new \App\Entity\Question();
+         $question1->setValue('En voyant ce post, quelle est ta réaction?');
+         $question1->setGoodAnswer('Je commente négativement');
+         $question1->setWrongAnswerOne('Je like');
+         $question1->setWrongAnswerTwo('Je commente positivement');
+         $question1->setWrongAnswerThree('J\'ignore');
+         $question1->setCategory($this->getReference('cat'));
+         $manager->persist($question1);
          $manager->flush();
+
+        $question2 = new \App\Entity\Question();
+        $question2->setValue('Le nouveau de 5B n\'est pas venu à l\'école cette semaine: Que ressens tu?');
+        $question2->setGoodAnswer('Coupable');
+        $question2->setWrongAnswerOne('Indifférent');
+        $question2->setWrongAnswerTwo('Triste');
+        $question2->setWrongAnswerThree('');
+        $question2->setCategory($this->getReference('cat'));
+        $manager->persist($question2);
+        $manager->flush();
+
+        $questions3 = new \App\Entity\Question();
+        $questions3->setValue('A sa place, qu\'aurais tu fais?');
+        $questions3->setGoodAnswer('Parler à tes parents');
+        $questions3->setWrongAnswerOne('Ne rien dire');
+        $questions3->setWrongAnswerTwo('En parler au personnel du collège');
+        $questions3->setWrongAnswerThree('Répondre aux commentaires');
+        $questions3->setCategory($this->getReference('cat'));
+        $manager->persist($questions3);
+        $manager->flush();
+
     }
 }
--- src/DataFixtures/User.php
+++ src/DataFixtures/User.php
@@ -23,7 +23,7 @@ class User extends Fixture
         $admin->setPassword($this->passwordEncoder->encodePassword($admin, 'Elneris.Dang2019'));
         $admin->setLogin('Elneris.Dang');
         $admin->setRoles(['ROLE_ADMIN']);
-        $admin->setCategoryStep(4);
+        $admin->setCategoryStep($this->getReference('cat'));
         $manager->persist($admin);
         $this->addReference('admin', $admin);
 
@@ -33,7 +33,7 @@ class User extends Fixture
         $eleve->setPassword($this->passwordEncoder->encodePassword($eleve, 'Jimmy.Achour2019'));
         $eleve->setLogin('Jimmy.Achour');
         $eleve->setRoles(['ROLE_USER']);
-        $eleve->setCategoryStep(1);
+        $eleve->setCategoryStep($this->getReference('cat'));
         $manager->persist($eleve);
         $this->addReference('eleve', $eleve);
         $manager->flush();
